49d2c03890f6c7c44c2715ccec2ebb664de67723
Easton Meth <easton.meth@gmail.com>
2023-07-12 17:34:45 -0400

    Add more campground info and update index styling

--- seeds/seed.js
+++ seeds/seed.js
@@ -19,14 +19,18 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)]
 const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
+        const price = Math.floor((Math.random() * 2000) + 1000) / 100
         const newCamp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${sample(cities).city}, ${sample(cities).state}`
+            location: `${sample(cities).city}, ${sample(cities).state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas soluta architecto tempore voluptatem quo quis, corrupti aliquid sapiente consequatur nam recusandae dolorem impedit, tempora assumenda perspiciatis iure labore rerum quia? Assumenda possimus obcaecati quos recusandae ex mollitia itaque debitis perspiciatis, ut deserunt ad iste commodi cupiditate! Molestias soluta, corporis saepe ex ab accusamus inventore, magni quisquam officia, dignissimos assumenda obcaecati.',
+            price
         })
         await newCamp.save()
     }
 }
 
 seedDB().then(() => {
-    mongo.connection.close()
+    mongoose.connection.close()
 })
